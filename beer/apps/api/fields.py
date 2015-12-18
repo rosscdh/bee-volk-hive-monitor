ee22bfca63b439e9ff4fe5cc038866c12d0811d1
Ross Crawford-d'Heureuse <rosscdh@s.local>
2015-12-18 10:31:28 +0100

    fixed lat lang needs to be decimal

--- beer/apps/api/fields.py
+++ beer/apps/api/fields.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
-
+from decimal import Decimal
 
 class GeopositionSerializerField(serializers.Field):
     DEFAULT_GEO_COORDS = ('51.1935462', '6.4479122999999845')
 
     def to_representation(self, obj):
         return {
-            'latitude': str(getattr(obj, 'latitude', self.DEFAULT_GEO_COORDS[0])),
-            'longitude': str(getattr(obj, 'longitude', self.DEFAULT_GEO_COORDS[1])),
+            'latitude': Decimal(getattr(obj, 'latitude', self.DEFAULT_GEO_COORDS[0])),
+            'longitude': Decimal(getattr(obj, 'longitude', self.DEFAULT_GEO_COORDS[1])),
         }
 
     def to_internal_value(self, data):
-        return '%s,%s' % (str(getattr(data, 'latitude', self.DEFAULT_GEO_COORDS[0])),
-                          str(getattr(data, 'longitude', self.DEFAULT_GEO_COORDS[1])))
+        return '%s,%s' % (Decimal(getattr(data, 'latitude', self.DEFAULT_GEO_COORDS[0])),
+                          Decimal(getattr(data, 'longitude', self.DEFAULT_GEO_COORDS[1])))
