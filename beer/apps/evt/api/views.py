50c02c1c87bdfd6c26c0aa0794309191cef75c59
Ross Crawford-d'Heureuse <rosscdh@s.local>
2015-12-18 15:12:47 +0100

    working on sensor structure

--- beer/apps/evt/api/views.py
+++ beer/apps/evt/api/views.py
@@ -3,10 +3,13 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status as http_status
 
-from rest_framework.permissions import AllowAny
-from ..signals.base import log_bet_event, log_influx_event
-
 from pinax.eventlog.models import Log
+from rest_framework.permissions import AllowAny
+
+from beer.apps.box.models import Box
+from beer.apps.sensor.models import Sensor
+
+from ..signals.base import log_bet_event, log_influx_event
 from .serializers import LogSerializer
 
 
@@ -29,15 +32,26 @@ class EventCreate(generics.ListCreateAPIView):
         sender = request_data.get('sender', None)
         sensor_action = request_data.get('sensor_action', None)
 
-        source = 'github' if request.META.get('X-Github-Event', None) is not None else None
+        device_id = request_data.get('tags', {}).get('device_id')
+        if device_id:
+            device, device_is_new = Box.objects.get_or_create(device_id=device_id)
 
+        sensor_id = request_data.get('tags', {}).get('sensor_id')
+        if sensor_id:
+            sensor, sensor_is_new = Sensor.objects.get_or_create(uuid=sensor_id)
+            if sensor_action:
+                for action in sensor_action.split(','):
+                    sensor.data[action] = request_data.get(action)
+                sensor.save(update_fields=['data'])
+
+        source = 'github' if request.META.get('X-Github-Event', None) is not None else None
         request_data['source'] = source
 
         if sender:
             request_data['original_sender'] = sender
 
         log_bet_event.send(sender=self, action='created', **request_data)
-        #import pdb;pdb.set_trace()
+
         if sensor_action is not None:
             log_influx_event.send(sender=self, action=sensor_action, **request_data)
 
