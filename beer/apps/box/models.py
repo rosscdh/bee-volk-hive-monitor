7849dd16fba14c8776e69df5b4a6495dbd02df75
Ross Crawford-d'Heureuse <rosscdh@s.local>
2016-01-06 09:32:54 +0100

    added version to box

--- beer/apps/box/models.py
+++ beer/apps/box/models.py
@@ -5,11 +5,20 @@ from shortuuidfield import ShortUUIDField as UUIDField
 from jsonfield import JSONField
 from geoposition.fields import GeopositionField
 
+from beer.utils.app import get_namedtuple_choices
+
+
+VERSIONS = get_namedtuple_choices('VERSIONS', (
+    (1, 'basic', 'Basic Sensor Kit'),
+    (2, 'wireless', 'v2 Wireless Device'),
+))
+
 
 class Box(models.Model):
     slug = UUIDField(auto=True,
                      db_index=True)
     owner = models.ForeignKey('auth.User', blank=True, null=True)
+    version = models.IntegerField(choices=VERSIONS.get_choices(), default=VERSIONS.basic)
     name = models.CharField(max_length=128, db_index=True, null=True, blank=True)
     mac_address = models.CharField(max_length=64, db_index=True, null=True, blank=True)
     device_id = models.CharField(max_length=64, db_index=True, null=True, blank=True)
