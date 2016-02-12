5803b1aa02c079332a99b3cade01e5f3b71b0e29
Ross Crawford-d'Heureuse <rosscdh@s.local>
2016-02-12 16:21:36 +0100

    removed redis task

--- beer/apps/box/tasks.py
+++ beer/apps/box/tasks.py
@@ -2,7 +2,7 @@
 from django_rq import job
 
 
-@job
-def process_basic_key_data_upload():
-    pass
-process_basic_key_data_upload.delay()  # Enqueue function in "default" queue
+# @job
+# def process_basic_key_data_upload():
+#     pass
+# process_basic_key_data_upload.delay()  # Enqueue function in "default" queue
